3fa02ab8344e38bf541039696d682638784f49ff
DESKTOP-LR78TFP\naymy <nay.myo.kyaw@ncs.com.sg>
2025-07-14 05:39:28 +0630

    End of section 6

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -16,12 +16,6 @@ namespace API.Controllers
             return members;
         }
 
-        /*         public ActionResult<IReadOnlyList<AppUser>> GetMembers()
-                                {
-                                    var member = context.Users.ToList();
-
-                                    return member;
-                                } */
         [Authorize]
         [HttpGet("{id}")] // localhost:5001/api/members/bob-id
         public async Task<ActionResult<AppUser>> GetMember(string id)
@@ -32,13 +26,5 @@ namespace API.Controllers
 
             return member;
         }
-/*         public ActionResult<AppUser> GetMember(string id)
-                {
-                    var member = context.Users.Find(id);
-
-                    if (member == null) return NotFound();
-
-                    return member;
-                } */
     }
 }
